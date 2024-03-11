2dc91a162c921255ae8c7a9137c67c9e90ee1639
mstent <mlstenton@gmail.com>
2024-03-11 10:21:38 +0000

    allow cross origin resource sharing

--- routes/api-router.js
+++ routes/api-router.js
@@ -2,14 +2,16 @@ const apiRouter = require("express").Router();
 const topicsRouter = require(`${__dirname}/topics-router`);
 const usersRouter = require(`${__dirname}/users-router`);
 const commentsRouter = require(`${__dirname}/comments-router`);
-const articlesRouter = require(`${__dirname}/articles-router`)
+const articlesRouter = require(`${__dirname}/articles-router`);
+const cors = require("cors");
 
 const {
     getEndpoints,
 } = require(`${__dirname}/../controllers/endpoints.controller`);
 
+apiRouter.use(cors());
 apiRouter.get("/", getEndpoints);
-apiRouter.use("/articles", articlesRouter)
+apiRouter.use("/articles", articlesRouter);
 apiRouter.use("/topics", topicsRouter);
 apiRouter.use("/users", usersRouter);
 apiRouter.use("/comments", commentsRouter);
